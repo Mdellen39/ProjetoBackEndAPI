3d8afea39dc151aac43f9ed5a204cd46114eac03
Mdellen39 <matheus.mrodrigues6@gmail.com>
2024-08-15 22:21:00 -0300

    Commit teste
    Commit teste

--- src/usuario/usuario.dm.ts
+++ src/usuario/usuario.dm.ts
@@ -93,7 +93,7 @@ export class UsuariosArmazenados{
             usuarioSalvo => usuarioSalvo.id !== id
         )
 
-        return usuario;     FSDFSDFSFS
+        return usuario; DAKODAKODSKADSOKDSAOKDSAO
     }
     
     //função para retornar todos os usuarios
